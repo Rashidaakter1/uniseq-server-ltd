3efa4aa885893576e79d83f92f8c2abcdb0126f2
Rashidaakter1 <rashidaakterchadni@gmail.com>
2022-05-24 22:09:42 +0600

    users

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
@@ -14,6 +15,22 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bczoi.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 async function run() {
     try {
@@ -72,13 +89,30 @@ async function run() {
       })
 
 
+      //user get api
 
-      //user post api 
-      app.post('/users',async(req,res)=>{
-        const userData=req.body;
-        const result = await usercollection.insertOne(userData)
+      app.get('/users',async(req,res)=>{
+        const result = await usercollection.find().toArray()
         res.send(result)
+      })
+  
+
+      //user put api 
+    app.put('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      console.log(user);
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usercollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ result, token });
+    
     })
+
 
 
     //reviews put api
